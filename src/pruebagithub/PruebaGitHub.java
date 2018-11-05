cd52e977fc73e02d021ddb5d4a4e65c0fd602180
Edugarpa <34040393+Edugarpa@users.noreply.github.com>
2018-11-05 21:36:06 +0100

    Update PruebaGitHub.java

--- src/pruebagithub/PruebaGitHub.java
+++ src/pruebagithub/PruebaGitHub.java
@@ -8,7 +8,7 @@ public class PruebaGitHub {
      */
     public static void main(String[] args) {
         System.out.println("Hola Mundo!!!");
-        int i 0 10;
+        int i = 10;
         if(i!=5){
             System.out.println("no es 5!");
         }
